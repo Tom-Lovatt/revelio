6b81869a66a346cf5f5cf781cf465e6be8650e67
Tom Lovatt <Tom_Lovatt@Mail.com>
2018-04-07 20:06:13 +0100

    Improved output, including progress updates

--- revelio.py
+++ revelio.py
@@ -20,6 +20,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
 TMP_DIR = os.path.join(tempfile.gettempdir(), 'revelio')
 
 SCORE_ALERT_THRESHOLD = 5
+PROGRESS_UPDATE_PERIOD = 10
 
 log = logging.getLogger(__name__)
 
@@ -27,12 +28,14 @@ log = logging.getLogger(__name__)
 def main():
     global SCORE_ALERT_THRESHOLD
 
-    start_time = time.clock()
-    signal.signal(signal.SIGINT, exit_handler)
     config = process_arguments()
+    config.start_time = time.time()
+    signal.signal(signal.SIGINT, exit_handler)
     configure_logger(config)
-
     SCORE_ALERT_THRESHOLD -= config.aggressive_scan
+    config.show_progress = not config.log_file
+
+    log.info("Searching for files...")
     target_files = enumerate_files(config.targets, config.recurse)
     results = scan(target_files, config)
 
@@ -41,7 +44,7 @@ def main():
         msg += " Did you mean to supply the --recurse flag?" if not config.recurse else ""
         log.warning(msg)
 
-    print_results(results, time.clock()-start_time)
+    print_results(results, time.time() - config.start_time)
 
 
 def exit_handler(signal, frame):
@@ -86,7 +89,16 @@ def scan(path_list, config):
     if config.git_root:
         processors.append(Git(config.git_root))
 
+    if config.show_progress:
+        path_list = list(path_list)
+        num_targets = sum(1 for x in path_list)
+        log.info("{} files and directories found. Scanning...".format(num_targets))
+
+    last_update = time.time()
+    scan_count = 0
+    suspicious_count = 0
     for path in path_list:
+        scan_count += 1
         if os.path.isdir(path):
             continue
         if not validate_file(path):
@@ -103,10 +115,32 @@ def scan(path_list, config):
                 log.debug("Running {} plugin".format(processor.get_processor_name()))
                 results[path].merge_with(processor.process(temp_path, path))
 
+        if results[path].score >= SCORE_ALERT_THRESHOLD:
+            suspicious_count += 1
+
+        if config.show_progress and time.time() > last_update + PROGRESS_UPDATE_PERIOD:
+            last_update = time.time()
+            print_progress(scan_count, num_targets, suspicious_count, config.start_time)
+
         os.unlink(temp_path)
         sleep((3 - config.priority) * 0.05)
 
     return results
+
+
+def print_progress(scan_count, total_count, suspicious_count, start_time):
+    completion = round((scan_count * 1.0 / total_count) * 100, 2)
+    completion_time = (time.time() - start_time) / scan_count * (total_count - scan_count)
+    if completion_time >= 60:
+        completion_time = int(completion_time / 60)
+        completion_time = str(completion_time) + (" minutes" if completion_time > 1 else " minute")
+    else:
+        completion_time = "<1 minute"
+
+    log.info("{} files scanned, {} suspicious files found, {}% complete. ".format(
+        scan_count, suspicious_count, completion)
+             + "Estimate {} until completion.".format(completion_time)
+             )
 
 
 def validate_file(path):
