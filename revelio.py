ac373c0d0b578a87eeb5f9e83c5e03aec552464b
Tom Lovatt <Tom_Lovatt@Mail.com>
2018-04-13 18:15:56 +0100

    Restructure main script file, keep functions in a more logical order

--- revelio.py
+++ revelio.py
@@ -29,7 +29,7 @@ def main() -> None:
     signal.signal(signal.SIGINT, exit_handler)
 
     log.info("Searching for files...")
-    target_files = enumerate_files(config.targets, config.recurse)
+    target_files = get_file_list(config.targets, config.recurse)
     results = scan(target_files, config)
 
     if len(results) == 0:
@@ -37,35 +37,58 @@ def main() -> None:
         msg += " Did you mean to supply the --recurse flag?" if not config.recurse else ""
         log.warning(msg)
 
-    print_results(results, time.time() - config.start_time)
+    print_results(results, time.time() - config.start_time, config.score_alert_threshold)
 
 
-def exit_handler() -> None:
-    log.critical("Caught exit signal, cleaning up and exiting.")
-    if os.path.isdir(TMP_DIR):
-        try:
-            shutil.rmtree(TMP_DIR)
-        except IOError as e:
-            log.error("Couldn't clean up temp files, failed to delete {}: {}".format(TMP_DIR, e))
+def process_arguments() -> Any:
+    parser = argparse.ArgumentParser(description='Scan for malicious PHP files, particularly those using obfuscation.')
+    parser.add_argument('-f', '--log-file', action='store', default=None,
+                        help="Send all output to the specified file.")
+    parser.add_argument('-r', '--recurse', action='store_true', default=False,
+                        help="Search recursively within the target directory.")
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help="Show all debugging statements.")
+    parser.add_argument('-g', '--git-root', action='store', default=False,
+                        help="If target is part of a Git repo, supply the root directory to "
+                             "use Git metadata in the scan. Note: This mode won't scan any "
+                             "files or directories listed in .gitignore")
+    parser.add_argument('-w', '--wordpress-root', action='store', default=False,
+                        help="If target is part of a Wordpress installation, specify "
+                             "the root directory to include hash verification in the scan")
+    parser.add_argument('-a', '--aggressive-scan', action='count', default=0,
+                        help="Lower the threshold for how suspicious a file needs to "
+                             "be before it's reported. Can be specified up to 3 times, "
+                             "increasing the level each time.")
+    parser.add_argument('-p', '--priority', action='store', default=2,
+                        help="Set the priority of the scan, from 0-3. Higher priorities "
+                             "will complete faster but use more system resources.",
+                        type=int, choices=[0, 1, 2, 3])
+    parser.add_argument(metavar="target directory", action="store", dest="targets", nargs='+')
 
-    sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    config = parser.parse_args()
+    config.start_time = time.time()
+    config.score_alert_threshold = SCORE_ALERT_THRESHOLD - config.aggressive_scan
+    config.show_progress = not config.log_file
+
+    return config
 
 
-def print_results(results: dict, duration: float, alert_threshold) -> None:
-    flagged = 0
-    for path, result in results.items():
-        if result.score >= alert_threshold:
-            log.info('{} was flagged with the following notes: {}\n'.format(path, ', '.join(result.rules)))
-            flagged += 1
-        else:
-            log.debug('{} file had a score of {} with the following rules: {}'.format(
-                path, result.score, ', '.join(result.rules)))
+def configure_logger(config: Any) -> None:
+    verbosity = logging.DEBUG if config.verbose else logging.INFO
+    fmt = '[%(asctime)s] [%(levelname)s] - %(message)s'
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    handlers = []
 
-    log.info("Scanned {} files in {}s, {} suspicious file(s) identified".format(
-        len(results),
-        round(duration, 2),
-        flagged
-    ))
+    if config.log_file:
+        handlers.append(logging.FileHandler(config.log_file))
+    else:
+        handlers.append(logging.StreamHandler())
+
+    logging.basicConfig(level=verbosity, format=fmt, datefmt=datefmt, handlers=handlers)
 
 
 def get_processors(config: Any) -> List[BaseProcessor]:
@@ -84,6 +107,36 @@ def get_processors(config: Any) -> List[BaseProcessor]:
         processors.append(GitProcessor(config.git_root))
 
     return processors
+
+
+def get_file_list(paths: List[str], recursive=False) -> List[str]:
+    all_files = []
+    for path in paths:
+        if os.path.islink(path):
+            log.warning("Found a symlink at {}, not following.".format(path))
+            continue
+        if os.path.isfile(path):
+            all_files.append(path)
+            continue
+
+        for dir_path, dir_names, filenames in os.walk(path):
+            for dir_name in dir_names:
+                full_dir_path = os.path.join(dir_path, dir_name)
+                if os.path.islink(full_dir_path):
+                    log.warning("Found a symlink at {}, not following.".format(full_dir_path))
+            for filename in filenames:
+                all_files.append(os.path.join(dir_path, filename))
+            if not recursive:
+                break
+
+    return all_files
+
+
+def validate_file(path: str) -> bool:
+    return (
+        '.php' in path.lower() and
+        os.path.isfile(path)
+    )
 
 
 def scan(path_list: list, config) -> dict:
@@ -145,85 +198,32 @@ def print_progress(scanned: int, total: int, suspicious: int, start_time: float)
              )
 
 
-def validate_file(path: str) -> bool:
-    return (
-        '.php' in path.lower() and
-        os.path.isfile(path)
-    )
+def print_results(results: dict, duration: float, alert_threshold) -> None:
+    flagged = 0
+    for path, result in results.items():
+        if result.score >= alert_threshold:
+            log.info('{} was flagged with the following notes: {}\n'.format(path, ', '.join(result.rules)))
+            flagged += 1
+        else:
+            log.debug('{} file had a score of {} with the following rules: {}'.format(
+                path, result.score, ', '.join(result.rules)))
+
+    log.info("Scanned {} files in {}s, {} suspicious file(s) identified".format(
+        len(results),
+        round(duration, 2),
+        flagged
+    ))
 
 
-def process_arguments() -> Any:
-    parser = argparse.ArgumentParser(description='Scan for malicious PHP files, particularly those using obfuscation.')
-    parser.add_argument('-f', '--log-file', action='store', default=None,
-                        help="Send all output to the specified file.")
-    parser.add_argument('-r', '--recurse', action='store_true', default=False,
-                        help="Search recursively within the target directory.")
-    parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                        help="Show all debugging statements.")
-    parser.add_argument('-g', '--git-root', action='store', default=False,
-                        help="If target is part of a Git repo, supply the root directory to "
-                             "use Git metadata in the scan. Note: This mode won't scan any "
-                             "files or directories listed in .gitignore")
-    parser.add_argument('-w', '--wordpress-root', action='store', default=False,
-                        help="If target is part of a Wordpress installation, specify "
-                             "the root directory to include hash verification in the scan")
-    parser.add_argument('-a', '--aggressive-scan', action='count', default=0,
-                        help="Lower the threshold for how suspicious a file needs to "
-                             "be before it's reported. Can be specified up to 3 times, "
-                             "increasing the level each time.")
-    parser.add_argument('-p', '--priority', action='store', default=2,
-                        help="Set the priority of the scan, from 0-3. Higher priorities "
-                             "will complete faster but use more system resources.",
-                        type=int, choices=[0, 1, 2, 3])
-    parser.add_argument(metavar="target directory", action="store", dest="targets", nargs='+')
+def exit_handler() -> None:
+    log.critical("Caught exit signal, cleaning up and exiting.")
+    if os.path.isdir(TMP_DIR):
+        try:
+            shutil.rmtree(TMP_DIR)
+        except IOError as e:
+            log.error("Couldn't clean up temp files, failed to delete {}: {}".format(TMP_DIR, e))
 
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-
-    config = parser.parse_args()
-    config.start_time = time.time()
-    config.score_alert_threshold = SCORE_ALERT_THRESHOLD - config.aggressive_scan
-    config.show_progress = not config.log_file
-
-    return config
-
-
-def configure_logger(config: Any) -> None:
-    verbosity = logging.DEBUG if config.verbose else logging.INFO
-    fmt = '[%(asctime)s] [%(levelname)s] - %(message)s'
-    datefmt = '%Y-%m-%d %H:%M:%S'
-    handlers = []
-
-    if config.log_file:
-        handlers.append(logging.FileHandler(config.log_file))
-    else:
-        handlers.append(logging.StreamHandler())
-
-    logging.basicConfig(level=verbosity, format=fmt, datefmt=datefmt, handlers=handlers)
-
-
-def enumerate_files(paths: List[str], recursive=False) -> List[str]:
-    all_files = []
-    for path in paths:
-        if os.path.islink(path):
-            log.warning("Found a symlink at {}, not following.".format(path))
-            continue
-        if os.path.isfile(path):
-            all_files.append(path)
-            continue
-
-        for dir_path, dir_names, filenames in os.walk(path):
-            for dir_name in dir_names:
-                full_dir_path = os.path.join(dir_path, dir_name)
-                if os.path.islink(full_dir_path):
-                    log.warning("Found a symlink at {}, not following.".format(full_dir_path))
-            for filename in filenames:
-                all_files.append(os.path.join(dir_path, filename))
-            if not recursive:
-                break
-
-    return all_files
+    sys.exit(1)
 
 
 if __name__ == '__main__':
